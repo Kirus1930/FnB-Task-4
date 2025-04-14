ec2eb2de36a3ae957685638d4fd696883e7bdfec
Kirus1930 <k_eryomin00@mail.ru>
2025-04-14 18:36:54 +0300

    Update server.js

--- http/frontend/server.js
+++ http/frontend/server.js
@@ -1,15 +1,12 @@
-// server.js (клиентская часть, порт 3500)
+const { ApolloServer } = require('apollo-server');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
-const { parse } = require('url');
-const { ApolloServer } = require('apollo-server');
-const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 const PORT = 3500;
 const PRODUCTS_PATH = path.join(__dirname, '../data/products.json');
 
-// GraphQL Schema
 const typeDefs = `
   type Product {
     id: Int!
@@ -30,7 +27,7 @@ const resolvers = {
       const data = await fs.readFile(PRODUCTS_PATH, 'utf8');
       return JSON.parse(data).map(product => 
         Object.fromEntries(fields.map(f => [f, product[f]]))
-      )
+      );
     }
   }
 };
@@ -38,21 +35,13 @@ const resolvers = {
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 const apolloServer = new ApolloServer({ schema });
 
-// HTTP Server
 const server = http.createServer(async (req, res) => {
-  const { pathname } = parse(req.url, true);
-  
-  if (pathname === '/graphql' && req.method === 'POST') {
+  if (req.url === '/graphql' && req.method === 'POST') {
     apolloServer.createHandler()(req, res);
-  } else if (pathname === '/' && req.method === 'GET') {
-    try {
-      const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(html);
-    } catch (err) {
-      res.writeHead(500);
-      res.end('Internal Server Error');
-    }
+  } else if (req.url === '/') {
+    const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
   } else {
     res.writeHead(404);
     res.end('Not Found');
