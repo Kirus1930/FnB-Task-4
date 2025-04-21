1bb91f4858cae5c3a194ab293d07cb72599a5032
Kirus1930 <k_eryomin00@mail.ru>
2025-04-21 16:27:00 +0300

    Update server.js

--- http/frontend/server.js
+++ http/frontend/server.js
@@ -1,13 +1,12 @@
-const { ApolloServer } = require('apollo-server');
-const { makeExecutableSchema } = require('@graphql-tools/schema');
 const http = require('http');
 const fs = require('fs').promises;
 const path = require('path');
+const { graphql, buildSchema } = require('graphql');
 
 const PORT = 3500;
 const PRODUCTS_PATH = path.join(__dirname, '../data/products.json');
 
-const typeDefs = `
+const schema = buildSchema(`
   type Product {
     id: Int!
     name: String!
@@ -19,35 +18,51 @@ const typeDefs = `
   type Query {
     products(fields: [String!]!): [Product!]!
   }
-`;
+`);
 
-const resolvers = {
-  Query: {
-    products: async (_, { fields }) => {
-      const data = await fs.readFile(PRODUCTS_PATH, 'utf8');
-      return JSON.parse(data).map(product => 
-        Object.fromEntries(fields.map(f => [f, product[f]]))
-      );
-    }
+const root = {
+  products: async ({ fields }) => {
+    const data = await fs.readFile(PRODUCTS_PATH, 'utf8');
+    return JSON.parse(data).map(product => 
+      Object.fromEntries(fields.map(f => [f, product[f]]))
+    );
   }
 };
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-const apolloServer = new ApolloServer({ schema });
-
 const server = http.createServer(async (req, res) => {
-  if (req.url === '/graphql' && req.method === 'POST') {
-    apolloServer.createHandler()(req, res);
-  } else if (req.url === '/') {
-    const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(html);
-  } else {
-    res.writeHead(404);
-    res.end('Not Found');
+  try {
+    if (req.url === '/graphql' && req.method === 'POST') {
+      let body = '';
+      for await (const chunk of req) {
+        body += chunk;
+      }
+      
+      const { query, variables } = JSON.parse(body);
+      const result = await graphql({
+        schema,
+        source: query,
+        rootValue: root,
+        variableValues: variables
+      });
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(result));
+    } 
+    else if (req.url === '/' && req.method === 'GET') {
+      const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(html);
+    } 
+    else {
+      res.writeHead(404);
+      res.end('Not Found');
+    }
+  } catch (err) {
+    res.writeHead(500);
+    res.end(err.message);
   }
 });
 
 server.listen(PORT, () => {
-  console.log(`Frontend server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
